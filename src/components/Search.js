05e33cce66953e5584cd0d3f50f3802ef5b11146
cz <deutschessz@gmail.com>
2019-02-20 22:03:23 +0800

    Added Load More button in Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,15 +8,26 @@ class Search extends React.Component {
   state = {
     fullSearchPodcasts: [],
     fullSearchEpisodes: [],
+    offsetEpisodes: 0,
+    totalEpisodeMatches: 0,
+    offsetPodcasts: 0,
+    totalPodcastMatches: 0,
     fliter: 'episodes',
     calledFullSearchEpisodes: false,
     calledFullSearchPodcasts: false
   };
 
+  loadMoreMatches = () => {
+    if (this.state.fliter === 'episodes' && this.state.fullSearchEpisodes.length <= this.state.totalEpisodeMatches) {
+      this.callFullSearchEpisodes(this.props.currentFullQuery);
+    }
+    if (this.state.fliter === 'podcasts' && this.state.fullSearchPodcasts.length <= this.state.totalPodcastMatches) {
+      this.callFullSearchPodcasts(this.props.currentFullQuery);
+    }
+  }
+
   callFullSearchEpisodes = (keywords) => {
-    console.log('calling api to search episodes');
-    console.log(keywords);
-    const endpoint = `https://api.listennotes.com/api/v1/search?sort_by_date=0&type=episode&offset=0&safe_mode=1&q=%22${keywords}%22`;
+    const endpoint = `https://api.listennotes.com/api/v1/search?sort_by_date=0&type=episode&offset=${this.state.offsetEpisodes}&safe_mode=1&q=%22${keywords}%22`;
     const request = {
       method: 'GET',
       headers: {
@@ -28,10 +39,19 @@ class Search extends React.Component {
       .then(res => res.json())
       .then(data => {
         console.log(data);
-        this.setState({
-          fullSearchEpisodes: [...data.results],
-          calledFullSearchEpisodes: true
-        });
+        if (!this.state.fullSearchEpisodes.length) {
+          this.setState({
+            fullSearchEpisodes: [...data.results],
+            offsetEpisodes: data.next_offset,
+            totalEpisodeMatches: data.total,
+            calledFullSearchEpisodes: true
+          });
+        } else {
+          this.setState({
+            fullSearchEpisodes: [...this.state.fullSearchEpisodes, ...data.results],
+            offsetEpisodes: data.next_offset
+          });
+        }
       })
       .catch(err => console.log(err));
   }
@@ -44,7 +64,7 @@ class Search extends React.Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.currentFullQuery !== prevProps.currentFullQuery) {
+    if (prevProps.currentFullQuery && this.props.currentFullQuery !== prevProps.currentFullQuery) {
       const keywords = this.props.currentFullQuery;
       this.props.history.push(`/search/${keywords}`);
       this.setState({
@@ -59,6 +79,36 @@ class Search extends React.Component {
     }
   }
 
+  callFullSearchPodcasts = (keywords) => {
+    const endpoint = `https://api.listennotes.com/api/v1/search?sort_by_date=0&type=podcast&offset=${this.state.offsetPodcasts}&safe_mode=1&q=%22${keywords}%22`;
+    const request = {
+      method: 'GET',
+      headers: {
+        "X-RapidAPI-Key": apiKey,
+        "Accept": "application/json"
+      }
+    };
+    fetch(endpoint, request)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (!this.state.fullSearchPodcasts.length) {
+          this.setState({
+            fullSearchPodcasts: [...data.results],
+            offsetPodcasts: data.next_offset,
+            totalPodcastMatches: data.total,
+            calledFullSearchPodcasts: true
+          });
+        } else {
+          this.setState({
+            fullSearchPodcasts: [...this.state.fullSearchPodcasts, ...data.results],
+            offsetPodcasts: data.next_offset
+          });
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
   handleFilterChange = (e) => {
     if (e.target.id === "episodes") {
       this.setState({
@@ -69,36 +119,21 @@ class Search extends React.Component {
         fliter: 'podcasts'
       });
       if (!this.state.calledFullSearchPodcasts) {
-        const keywords = this.props.currentFullQuery;
-        const endpoint = `https://api.listennotes.com/api/v1/search?sort_by_date=0&type=podcast&offset=0&safe_mode=1&q=%22${keywords}%22`;
-        const request = {
-          method: 'GET',
-          headers: {
-            "X-RapidAPI-Key": apiKey,
-            "Accept": "application/json"
-          }
-        };
-        fetch(endpoint, request)
-          .then(res => res.json())
-          .then(data => {
-            console.log(data);
-            this.setState({
-              fullSearchPodcasts: [...data.results],
-              calledFullSearchPodcasts: true
-            });
-          })
-          .catch(err => console.log(err));
+        this.callFullSearchPodcasts(this.props.currentFullQuery);
       }
     }
   }
 
   render() {
+    const episodesActive = this.state.fliter === 'episodes' ? 'active' : '';
+    const podcastsActive = this.state.fliter === 'episodes' ? '' : 'active';
     const fullSearchEpisodes = this.state.fullSearchEpisodes;
     const fullSearchPodcasts = this.state.fullSearchPodcasts;
     let renderEpisodes;
     let renderPodcasts;
+    let loadMoreBtn;
 
-    if (fullSearchEpisodes.length) {
+    if (fullSearchEpisodes.length && fullSearchEpisodes.length <= this.state.totalEpisodeMatches) {
       renderEpisodes = fullSearchEpisodes.map((episode) => (
         <EpisodeCardStyleB
           key={episode.id}
@@ -110,11 +145,19 @@ class Search extends React.Component {
           clearInputInHeader={this.props.clearInputInHeader}
         />
       ));
+      loadMoreBtn = (
+        <button
+          className="load-more"
+          onClick={this.loadMoreMatches}
+        >
+          Load More
+        </button>
+      );
     } else if (this.state.calledFullSearchEpisodes) {
       renderEpisodes = (<div className="no-match-prompt">Oops... No matched results found.</div>);
     }
 
-    if (fullSearchPodcasts.length) {
+    if (fullSearchPodcasts.length && fullSearchPodcasts.length <= this.state.totalPodcastMatches) {
       renderPodcasts = fullSearchPodcasts.map((match) => (
         <PodcastCardStyleC
           podcast={match}
@@ -122,13 +165,21 @@ class Search extends React.Component {
           clearInputInHeader={this.props.clearInputInHeader}
         />
       ));
+      loadMoreBtn = (
+        <button
+          className="load-more"
+          onClick={this.loadMoreMatches}
+        >
+          Load More
+        </button>
+      );
     } else if (this.state.calledFullSearchPodcasts) {
       renderPodcasts = (<div className="no-match-prompt">Oops... No matched results found.</div>);
     }
 
+    // important to assign value to matches after computation is done.
     const matches = this.state.fliter === 'episodes' ?  renderEpisodes : renderPodcasts;
-    const episodesActive = this.state.fliter === 'episodes' ? 'active' : '';
-    const podcastsActive = this.state.fliter === 'episodes' ? '' : 'active';
+
     return (
       <div className="page-container">
         <div className="matched-container2">
@@ -151,6 +202,9 @@ class Search extends React.Component {
           </div>
           <div className="matched-results">
             {matches}
+          </div>
+          <div>
+            {loadMoreBtn}
           </div>
         </div>
       </div>
